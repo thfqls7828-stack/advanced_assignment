56f3a482d43153662579a98bf320d3f3b54ebd06
glory <thfqls7828@gmail.com>
2025-12-09 18:05:34 +0900

    fix : 반복문 수정

--- lib/print_student_info.dart
+++ lib/print_student_info.dart
@@ -6,16 +6,17 @@ import 'package:advanced_assignment/score_student.dart';
 // 학생 이름을 입력 받아 해당 학생의 데이터를 출력하는 함수
 ScoreStudent printStudentInfo(List<ScoreStudent> studentList) {
   while (true) {
-    for (ScoreStudent studentInfo in studentList) {
-      print("어떤 학생의 점수를 확인하시겠습니까?");
-      String? name = stdin.readLineSync(encoding: utf8);
+    print("어떤 학생의 점수를 확인하시겠습니까?");
+    String? name = stdin.readLineSync(encoding: utf8);
 
-      if (studentInfo.name == name) {
-        studentInfo.showInfo();
-        return studentInfo;
-      } else {
-        print("잘못된 학생 이름을 입력하셨습니다. 다시 입력해주세요.");
-      }
+    if (studentList.any((student) => student.name == name)) {
+      final ScoreStudent student = studentList.firstWhere(
+        (student) => student.name == name,
+      );
+      student.showInfo();
+      return student;
+    } else {
+      print("잘못된 학생 이름을 입력하셨습니다. 다시 입력해주세요.");
     }
   }
 }
